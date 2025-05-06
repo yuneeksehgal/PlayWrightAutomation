eb847c30046bccee939023c4f7b8493f1f3f2eaa
Yuneek Sehgal <yuneeksehgal@gmail.com>
2025-05-06 11:37:55 +0530

    Update NetworlTest2.spec.js

--- tests/NetworlTest2.spec.js
+++ tests/NetworlTest2.spec.js
@@ -5,8 +5,8 @@ test('@QW Security test request intercept', async ({ page }) => {
 
     //login and reach orders page
     await page.goto("https://rahulshettyacademy.com/client");
-    await page.locator("#userEmail").fill("anshika@gmail.com");
-    await page.locator("#userPassword").fill("Iamking@000");
+    await page.locator("#userEmail").fill("yuneeksehgal@gmail.com");
+    await page.locator("#userPassword").fill("Testing1@3");
     await page.locator("[value='Login']").click();
     await page.waitForLoadState('networkidle');
     await page.locator(".card-body b").first().waitFor();
