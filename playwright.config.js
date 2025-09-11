01272d8a102b5cd02e17f1be4157e446dbf77da1
Yuneek Sehgal <yuneeksehgal@gmail.com>
2025-09-11 14:14:49 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,31 +1,46 @@
 // @ts-check
-const { devices } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test');
 
-const config = {
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+// This configuration file is used to define the settings for Playwright tests. 
+// It includes settings for the test directory, timeout, expect timeout, reporter, and browser context options.
+module.exports = defineConfig({
   testDir: './tests',
-  retries :0,
-  
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  workers: 5,
+  timeout: 100*1000,
   expect: {
-  
-    timeout: 5000
+    timeout: 40*1000,
   },
-  
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    //reporter: [["line"], ["allure-playwright"]],
   use: {
+   
+     browserName: "chromium",
+     userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+     headless : false,
+     screenshot : 'on',
+     trace : 'on',
+     video: 'retain-on-failure',
+     
+     // Below are the 3 configs to maximize the browser window
+     viewport: null,
+     launchOptions: {
+       args: ['--start-maximized'],
+     },
+     deviceScaleFactor: undefined
 
-    browserName : 'chromium',
-    headless : true,
-    screenshot : 'on',
-    trace : 'on',//off,on
-    
-    
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     
   },
 
+  
+});
 
-};
-
-module.exports = config;
